5c1371571a9c7b9029223942f66abda71be1affb
Christopher Adams <cdadams1888@yahoo.com>
2016-11-15 16:02:53 -0500

    c# methods

---
+++ moreMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace moreMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your first name: ");
+            string firstName = Console.ReadLine();
+            testMethod(firstName);
+        }
+
+        private static void testMethod(string firstName)
+        {
+            Console.Write("Hello {0}! This is inside of the method.", firstName);
+            Console.ReadLine();
+        }
+    }
+}
