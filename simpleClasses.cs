22cb007012d582659eeffbbb2bfd8896b4a1c26b
Christopher Adams <cdadams1888@yahoo.com>
2016-11-15 16:03:43 -0500

    classes in C#

---
+++ simpleClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Yaris";
+            myCar.Year = 2007;
+            myCar.Color = "Silver";
+            decimal value = determineMarketValue(myCar);
+            // Console.WriteLine("{0} {1} {2} {3}", myCar.Make, myCar.Model, myCar.Year, myCar.Color);
+            Console.WriteLine("{0:C}",value);
+            Console.ReadLine();
+
+        }
+
+        private static decimal determineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+            return carValue;
+        }
+    }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+    }
+}
