713b9439b9d561fe8d81a24ca7a824fcfa5ee887
Christopher Adams <cdadams1888@yahoo.com>
2016-11-15 15:56:46 -0500

    Various C# exercises

---
+++ nameBackwards.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace messin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName     = "Christopher";
+            int lengthCounter    = firstName.Length;
+            var backwardsName    = "";
+            int[] backwardsArray = new int[] { };
+            for (int i = 0; i < firstName.Length; i++)
+            {
+                lengthCounter--;
+                backwardsName += firstName[lengthCounter];
+                
+            }
+            Console.Write(backwardsName);
+            Console.ReadLine();
+        }
+    }
+}
+
+
